7f3ca99bb72e4a421c065483a59532a423be9d77
Aparna Krishnan <aparna.k@vonnue.com>
2024-02-28 19:46:22 +0530

    complete 3D portfolio website

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,72 +14,74 @@ import {
   vonnue,
 } from "../assets/icons";
 
+import { vinglish } from "../assets/images";
+
 export const skills = [
   {
     imageUrl: css,
     name: "CSS",
     type: "Frontend",
-    url: "https://developer.mozilla.org/en-US/docs/Web/CSS"
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
   },
   {
     imageUrl: git,
     name: "Git",
     type: "Version Control",
-    url:"https://git-scm.com/doc"
+    url: "https://git-scm.com/doc",
   },
   {
     imageUrl: github,
     name: "GitHub",
     type: "Version Control",
-    url: "https://docs.github.com/en"
+    url: "https://docs.github.com/en",
   },
   {
     imageUrl: html,
     name: "HTML",
     type: "Frontend",
-    url:"https://developer.mozilla.org/en-US/docs/Web/HTML"
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
   },
   {
     imageUrl: javascript,
     name: "JavaScript",
     type: "Frontend",
-    url:"https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
   },
   {
     imageUrl: nextjs,
     name: "Next.js",
     type: "Frontend",
-    url: "https://nextjs.org/docs"
+    url: "https://nextjs.org/docs",
   },
   {
     imageUrl: react,
     name: "React",
     type: "Frontend",
-    url:"https://react.dev/"
+    url: "https://react.dev/",
   },
   {
     imageUrl: tailwindcss,
     name: "Tailwind CSS",
     type: "Frontend",
-    url:"https://v2.tailwindcss.com/docs"
+    url: "https://v2.tailwindcss.com/docs",
   },
   {
     imageUrl: typescript,
     name: "TypeScript",
     type: "Frontend",
-    url:"https://www.typescriptlang.org/docs/"
+    url: "https://www.typescriptlang.org/docs/",
   },
   {
     imageUrl: react,
     name: "React Native",
     type: "Frontend",
-    url:"https://reactnative.dev/docs/getting-started"
+    url: "https://reactnative.dev/docs/getting-started",
   },
   {
     imageUrl: wordpress,
     name: "Wordpress",
     type: "Frontend",
-    url:"https://wordpress.org/documentation/"
+    url: "https://wordpress.org/documentation/",
   },
 ];
 
@@ -144,4 +146,12 @@ export const socialLinks = [
 ];
 
 export const projects = [
+  {
+    name: "Vinglish App",
+    description:
+      "Developed an android application using react native that helps to learn english vocabulary and revise it, through entertainment activities like games, quiz etc",
+    link: "https://github.com/Aparnakrishnan-Vonnue/Vinglish-App",
+    theme: "btn-back-pink",
+    iconUrl: vinglish,
+  },
 ];
